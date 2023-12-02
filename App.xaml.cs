4065df8ae97a684d8c6c94cd239020ffab5ed22a
BharathRam125 <bharathram1200@gmail.com>
2023-12-02 22:28:21 +0530

    Project created

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickPeer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
