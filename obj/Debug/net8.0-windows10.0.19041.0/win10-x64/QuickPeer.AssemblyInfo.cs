0d4bf045619910e4a31c43854cf7e769149f436d
BharathRam125 <bharathram1200@gmail.com>
2023-12-28 10:59:25 +0530

    #1  Bottom tab navigation bar

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/QuickPeer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/QuickPeer.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickPeer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6496636fdccd0c1b660b124fa6bc9ea6fc54a843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+727fc8723742b765a76c78be18d4ce886280ed69")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickPeer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickPeer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
