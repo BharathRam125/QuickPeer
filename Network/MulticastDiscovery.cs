4a6558c62ffe790abd7ba6e8283997af866f3865
BharathRam125 <bharathram1200@gmail.com>
2023-12-31 23:07:50 +0530

    created Network directory

---
+++ Network/MulticastDiscovery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace QuickPeer.Network
+{
+    internal class MulticastDiscovery
+    {
+        private const int MulticastPort = 52100;
+        private const string MulticastAddress = "224.0.0.125";
+        private static List<string> _nearbyDevices = [];
+
+         public static List<string> NearbyDevices()
+        {
+            _nearbyDevices.Add("Test 1");
+            _nearbyDevices.Add("Test 2");
+
+            return _nearbyDevices;
+        }
+
+    }
+}
